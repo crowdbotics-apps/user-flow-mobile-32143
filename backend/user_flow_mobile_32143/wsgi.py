58849619d05f7cd0203fecab00975e4049f161b8
Crowdbotics <team@crowdbotics.com>
2021-11-25 17:14:47 +0000

    Initial commit.

---
+++ backend/user_flow_mobile_32143/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for user_flow_mobile_32143 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_flow_mobile_32143.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "user_flow_mobile_32143",
+  url: "https://user_flow_mobile_32143.botics.co",
+  api: "https://user_flow_mobile_32143.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
